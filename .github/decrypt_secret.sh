49ff72c75a6e834442eac32851fcc3c9545247d1
maicon <maicon@peeledfinance.com>
2019-12-02 01:18:48 +0000

    try fix

--- .github/decrypt_secret.sh
+++ .github/decrypt_secret.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-gpg --quiet --batch --yes --decrypt --passphrase="$SAFE_KEY" \
+gpg --quiet --batch --yes --decrypt --passphrase="${SAFE_KEY}" \
 --output charts/templates/configmap.yaml secrets/configmap.yaml.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$SAFE_KEY" \
---output $HOME/.kube/config secrets/config.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="${SAFE_KEY}" \
+--output ${HOME}/.kube/config secrets/config.gpg
 
 # move this to a kubernetes secret
-gpg --quiet --batch --yes --decrypt --passphrase="$SAFE_KEY" \
+gpg --quiet --batch --yes --decrypt --passphrase="${SAFE_KEY}" \
 --output certs/fullchain.pem secrets/fullchain.pem.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$SAFE_KEY" \
+gpg --quiet --batch --yes --decrypt --passphrase="${SAFE_KEY}" \
 --output certs/privkey.pem secrets/privkey.pem.gpg

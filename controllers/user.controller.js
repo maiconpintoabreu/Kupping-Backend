ba349940e4277caad42f8b31d3a695e841b3db09
Maicon <maiconspas@gmail.com>
2020-04-17 15:28:39 +0100

    fake user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,7 +8,7 @@ exports.getUsers = function (req, res) {
         res.status(200).send(Users || []);
      });
 };
-exports.login = function (req, res) {
+var login = function (req, res) {
     console.log("Login...",req.body.username);
     User.findOne({"username":req.body.username}, function(err, user) {
         if(err)
@@ -29,41 +29,46 @@ exports.login = function (req, res) {
         }
      });
 };
+exports.login = login
 exports.getUserById = function(id) {
     return User.findOne({_id:id});
 }
 exports.insertUser = function (req, res) {
-    const user = new User({
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password,
-        company: req.body.company,
-        attendee: req.body.attendee || false,
-        organizer: req.body.organizer || false
-    });
-    User.findOne({$or:[{email:user.email},{username:user.username}]}).then(resUser=>{
-        if(!resUser){
-            user.save(function (err, results) {
-                if(err) {
-                    console.error("Error",err);
-                    res.status(500).send(err);
-                }else{
-                    const token = jwt.sign({
-                        data: {id:results._id,organizer:results.organizer,attendee:results.attendee}
-                    }, jwtSecret, { expiresIn: '24h' });
-                    res.status(200).json({token:token});
+    if(req.body.username === "test12345678" && !req.insertUser) {
+        login(req,res);
+    }else{
+        const user = new User({
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password,
+            company: req.body.company,
+            attendee: req.body.attendee || false,
+            organizer: req.body.organizer || false
+        });
+        User.findOne({$or:[{email:user.email},{username:user.username}]}).then(resUser=>{
+            if(!resUser){
+                user.save(function (err, results) {
+                    if(err) {
+                        console.error("Error",err);
+                        res.status(500).send(err);
+                    }else{
+                        const token = jwt.sign({
+                            data: {id:results._id,organizer:results.organizer,attendee:results.attendee}
+                        }, jwtSecret, { expiresIn: '24h' });
+                        res.status(200).json({token:token});
+                    }
+                });
+            }else{
+                if(resUser.email == user.email && resUser.username == user.username){
+                    res.status(403).send("Email and Username already exists");
+                }else if(resUser.email == user.email){
+                    res.status(403).send("Email already exists");
+                }else if(resUser.username == user.username){
+                    res.status(403).send("Username already exists");
                 }
-            });
-        }else{
-            if(resUser.email == user.email && resUser.username == user.username){
-                res.status(403).send("Email and Username already exists");
-            }else if(resUser.email == user.email){
-                res.status(403).send("Email already exists");
-            }else if(resUser.username == user.username){
-                res.status(403).send("Username already exists");
             }
-        }
-    }).catch(errUser=>{
-        res.status(500).json({error:errUser.message});
-    });
+        }).catch(errUser=>{
+            res.status(500).json({error:errUser.message});
+        });
+    }
 };
